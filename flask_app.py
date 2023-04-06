76c839ae00051154a7d2997aae94c6bc6bd761bb
josf <jayiikdev@gmail.com>
2023-04-07 01:18:29 +0200

    new

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,27 @@
-from flask import Flask, redirect, request, flash, url_for, render_template, send_file
+from flask import Flask, redirect, request, session, url_for, render_template, send_file
 from werkzeug.utils import secure_filename
 from os import path, mkdir
 import shutil
 from misc import namesystem_random
 
 
+_="""
+
+TODO:
+
+* captcha 
+* tunnel persistence with cookies
+
+"""
+
+
 UPLOAD_FOLDER = "./cache/"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-APP_URL = "localhost:5000/"
+APP_URL = "http://f54b-31-30-166-249.ngrok.io/"
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config["secret_key"] = "sdsajldnasjdjasdjaks"
+app.secret_key = "sdsajsldnasjdjasdjaks"
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -21,14 +31,30 @@ open_tunnels = []
 
 @app.route("/")
 def main():
-    return render_template("new_index.html")
+
+    krtinec = False
+
+    try:
+        krtinec = session["krtinec"]
+    except KeyError:
+        pass
+
+    return render_template("new_index.html", e=bool(krtinec), krtinec=krtinec)
 
 @app.route("/new")
 def new():
-    new_id = namesystem_random()
-    mkdir(path.join(app.config["UPLOAD_FOLDER"], new_id))
-    open_tunnels.append(new_id)
-    return redirect("/host/"+new_id)
+
+    try:
+        
+        return redirect(session["krtinec"])
+    
+    except KeyError:
+
+        new_id = namesystem_random()
+        mkdir(path.join(app.config["UPLOAD_FOLDER"], new_id))
+        open_tunnels.append(new_id)
+        session["krtinec"] = APP_URL+"host/"+new_id
+        return redirect("/host/"+new_id)
 
 @app.route("/host/<id>")
 def host(id):
@@ -47,8 +73,8 @@ def upload_file(id):
         if request.method == 'POST':
             # check if the post request has the file part
             if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
+                e = 'No file part'
+                return redirect(url_for("upload_file", id=id, state=e))
             files = request.files.getlist("file")
             # If the user does not select a file, the browser submits an
             # empty file without a filename.
@@ -56,15 +82,15 @@ def upload_file(id):
             for file in files:
 
                 if file.filename == '':
-                    flash('No selected file')
-                    return redirect(request.url)
+                    e = 'No selected file'
+                    return redirect(url_for("upload_file", id=id, state=e))
 
                 if file and allowed_file(file.filename):
 
                     filename = secure_filename(file.filename)
                     file.save(path.join(app.config['UPLOAD_FOLDER'], id+"/"+filename))
             
-            return redirect(url_for("success"))
+            return redirect(url_for("upload_file", id=id, state="w"))
 
         return render_template("new_submit.html", id=id)
     
@@ -87,6 +113,10 @@ def download(id):
 
 @app.route("/discard/<id>")
 def discard(id):
+    try:
+        session.pop("krtinec", default=None)
+    except Exception:
+        pass
     if not (id in open_tunnels):
         return redirect("/")
     else:
